84618526afda1a4d9c621979ba999bf6b10e75cf
kriti022 <47914821+kriti022@users.noreply.github.com>
2026-04-16 01:24:28 -0500

    Update OldPhonePadTests.cs

--- OldPhonePadTests.cs
+++ OldPhonePadTests.cs
@@ -1,1 +1,48 @@
+using Xunit;
 
+public class OldPhonePadTests
+{
+    private readonly OldPhonePadDecoder _decoder = new();
+
+    [Fact]
+    public void BasicInput_ShouldReturnCorrectLetter()
+    {
+        Assert.Equal("E", _decoder.Decode("33#"));
+    }
+
+    [Fact]
+    public void ShouldHandleBackspace()
+    {
+        Assert.Equal("B", _decoder.Decode("227*#"));
+    }
+
+    [Fact]
+    public void ShouldHandleWord()
+    {
+        Assert.Equal("HELLO", _decoder.Decode("4433555 555666#"));
+    }
+
+    [Fact]
+    public void ShouldHandlePauseBetweenSameDigits()
+    {
+        Assert.Equal("CA", _decoder.Decode("222 2#"));
+    }
+
+    [Fact]
+    public void ShouldHandleMultipleBackspaces()
+    {
+        Assert.Equal("", _decoder.Decode("22**#"));
+    }
+
+    [Fact]
+    public void ShouldWrapAroundLetters()
+    {
+        Assert.Equal("Z", _decoder.Decode("9999#"));
+    }
+
+    [Fact]
+    public void EmptyInput_ShouldReturnEmpty()
+    {
+        Assert.Equal("", _decoder.Decode("#"));
+    }
+}
